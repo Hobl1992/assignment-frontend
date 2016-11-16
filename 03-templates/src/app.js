eba0b3f5c0b478a61f3da3077df64c5a56239e93
Tobias Gann <tgann.mmt-b2015@fh-salzburg.ac.at>
2016-11-17 00:07:59 +0100

    added playerobjects

--- 03-templates/src/app.js
+++ 03-templates/src/app.js
@@ -1,40 +1,51 @@
-import $ from 'jquery'
-import router from './router'
-import homeTpl from './templates/home.hbs'
-import magnusTpl from './templates/magnus.hbs'
-import sergeyTpl from './templates/sergey.hbs'
-import contactTpl from './templates/contact.hbs'
-import notFoundTpl from './templates/not-found.hbs'
+import $ from 'jquery';
+import router from './router';
+import homeTpl from './templates/home.hbs';
+import contactTpl from './templates/contact.hbs';
+import notFoundTpl from './templates/not-found.hbs';
+import playerTpl from './templates/player.hbs';
 
-const $app = $('#app')
+const $app = $('#app');
+
+const chessPlayers = {
+    'magnus' : {
+        name: 'Magnus Carlsen',
+        image: 'https://cdn.worldchess.com/static/img/ny2016v2/carlsen.png'
+        description: 'is good but not very good'
+    },
+    'sergey' : {
+        name: 'Sergey Karjakin',
+        image: 'https://cdn.worldchess.com/static/img/ny2016v2/karjakin.png'
+        description: 'is better but also not very good'
+    }
+}
 
 function index() {
-  $app.html(homeTpl())
+  $app.html(homeTpl());
 }
 
 function contact() {
-  $app.html(contactTpl())
-}
-
-function players(ctx) {
-  let tpl = () => {}
-  switch (ctx.params.player) {
-    case 'magnus':
-      tpl = magnusTpl
-      break;
-    case 'sergey':
-      tpl = sergeyTpl
-      break;
-  }
-  $app.html(tpl())
+  $app.html(contactTpl());
 }
 
 function notFound() {
-  $app.html(notFoundTpl())
+  $app.html(notFoundTpl());
 }
 
-router('/', index)
-router('/players/:player', players)
-router('/contact', contact)
-router('*', notFound)
-router()
+function players(dynamicPart) {
+    if(dynamicPart === 'sergey') {
+        $app.html(playerTpl(chessPlayers.sergey));
+    }
+    else if(dynamicPart === 'magnus') {
+        $app.html(playerTpl(chessPlayers.magnus));
+    }
+    else{
+        $app.html(notFoundTpl());
+    }
+}
+
+router('/', index);
+router('/contact', contact);
+router('/players/:player', players);
+router('*', notFound);
+router();
