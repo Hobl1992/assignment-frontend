e39de36e610b4a6be3bc58521f55d1e6c746c9f3
Tobias Gann <tgann.mmt-b2015@fh-salzburg.ac.at>
2017-01-09 00:34:25 +0100

    added 05-svg

--- 05-svg/src/app.js
+++ 05-svg/src/app.js
@@ -7,6 +7,7 @@ import textTpl from './templates/text.hbs'
 import interactionTpl from './templates/interaction.hbs'
 import notFoundTpl from './templates/not-found.hbs'
 
+
 const app = document.getElementById('app')
 
 function index() {
@@ -30,12 +31,57 @@ function text() {
 }
 
 function interaction() {
-  app.innerHTML = interactionTpl()
+    app.innerHTML = interactionTpl()
+
+  let backgroundBtn = document.getElementById('btn-background'),
+      backgroundBtnWidth = backgroundBtn.getAttribute('width'),
+      slider = document.getElementById('slider'),
+      slideBtn = document.getElementById('btn-slide'),
+      slideBtnWidth = slideBtn.getAttribute('width'),
+      arrowBtn = document.getElementById('btn-arrow'),
+      slideBtnPos = 0,
+      actPosMouseX = 0,
+      committed = false,
+      mouseDown = false;
+
+  slider.addEventListener('mousedown', function (e) {
+    mouseDown = true;
+    actPosMouseX = e.clientX;
+  });
+
+  document.addEventListener('mouseup', function (e) {
+    mouseDown = false;
+    if(slideBtnPos >= backgroundBtnWidth - 5) {
+      slideBtn.setAttribute('width', backgroundBtnWidth);
+      committed = true;
+      let committed = document.getElementById('committed');
+      committed.setAttribute('visibility', 'visible');
+    }
+    else {
+      slideBtn.setAttribute('width', slideBtnWidth);
+      arrowBtn.setAttribute('transform','translate(0,0)');
+    }
+  });
+
+  slider.addEventListener('mousemove', function (e) {
+    if(!mouseDown || committed)
+      return;
+
+    let movementX = parseInt(e.clientX - actPosMouseX);
+    actPosMouseX += movementX;
+    slideBtnPos = parseInt(actPosMouseX - slideBtnWidth);
+
+    if(slideBtnPos >= slideBtnWidth && slideBtnPos <= backgroundBtnWidth) {
+      slideBtn.setAttribute('width', slideBtnPos);
+      arrowBtn.setAttribute('transform',`translate(${slideBtnPos - slideBtnWidth},0)`);
+    }
+  });
 }
 
 function notFound() {
   app.innerHTML = notFoundTpl()
 }
+
 router('/', index)
 router('/simple', simple)
 router('/style', style)
